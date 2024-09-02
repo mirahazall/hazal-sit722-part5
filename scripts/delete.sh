b31d39003e51fa0cc6ccd8f2adc0a36e5f1d6404
mirahazall <s223530601@deakin.edu.au>
2024-09-02 11:16:59 +1000

    first commit

---
+++ scripts/delete.sh
@@ -0,0 +1,14 @@
+#
+# Destroys the Node.js microservice on Kubernetes.
+#
+# Environment variables:
+#
+#   CONTAINER_REGISTRY - The hostname of your container registry.
+#   VERSION - The version number of the image to deploy.
+#
+# Usage:
+#
+#   ./scripts/deploy.sh
+#
+
+envsubst < ./scripts/kubernetes/deploy.yaml | kubectl delete -f -
---
+++ variables.tf
@@ -0,0 +1,12 @@
+# Sets global variables for this Terraform project.
+
+variable app_name {
+  default = "deakinlibrary"
+}
+
+variable location {
+  default = "australiaeast"
+}
+
+variable kubernetes_version {    
+}
