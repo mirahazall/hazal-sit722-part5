3baa18a7220ec655ff791d1dc7ead509f7bb621c
mirahazall <s223530601@deakin.edu.au>
2024-09-03 10:13:41 +1000

    third commit

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -8,5 +8,5 @@
 
 
 #echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
-docker push $CONTAINER_REGISTRY/book_catalog:$VERSION
-docker push $CONTAINER_REGISTRY/inventory_management:$VERSION
+docker push ${CONTAINER_REGISTRY}/book_catalog:${VERSION}
+docker push ${CONTAINER_REGISTRY}/inventory_management:${VERSION}
