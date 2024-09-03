2d76a91b67f0f0954d6e1be728a9b28fa4268303
mirahazall <s223530601@deakin.edu.au>
2024-09-03 10:31:09 +1000

    third commit

--- inventory_management/app/database.py
+++ inventory_management/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = "postgresql://mirahazall:lwoutTXmwiEsemV3PjnA8meGKdXhzV4A@dpg-cr5aa3bv2p9s73afd9bg-a.oregon-postgres.render.com/part3"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
