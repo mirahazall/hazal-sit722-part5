5befae17e41f3e5ba95e2deeab639c519b848109
mirahazall <s223530601@deakin.edu.au>
2024-09-20 22:56:20 +1000

    adjusted the pipeline

--- resource-group.tf
+++ resource-group.tf
@@ -1,7 +1,7 @@
 #
 # Creates a resource group for FlixTube in your Azure account.
 #
-resource "azurerm_resource_group" "libraryapp" {
+resource "azurerm_resource_group" "bookapp" {
   name     = var.app_name
   location = var.location
 }
